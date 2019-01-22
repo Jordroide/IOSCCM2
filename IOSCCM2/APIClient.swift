f5d60c1769f46d1e76a2a1a326ab77095917c5be
Serreau Jordane <jordane.serreau@sylpheo.com>
2019-01-22 17:01:35 +0100

    Initial work, pojo and Controller

---
+++ IOSCCM2/APIClient.swift
@@ -0,0 +1,9 @@
+//
+//  APIClient.swift
+//  IOSCCM2
+//
+//  Created by Jordane SERREAU Dev on 22/01/2019.
+//  Copyright © 2019 Jordane SERREAU Dev. All rights reserved.
+//
+
+import Foundation
---
+++ IOSCCM2/Domains.swift
@@ -0,0 +1,9 @@
+//
+//  Domains.swift
+//  IOSCCM2
+//
+//  Created by Jordane SERREAU Dev on 22/01/2019.
+//  Copyright © 2019 Jordane SERREAU Dev. All rights reserved.
+//
+
+import Foundation

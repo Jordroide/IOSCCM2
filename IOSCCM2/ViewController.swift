c819eb4d04277541fe68033e1358e558ae719569
Serreau Jordane <jordane.serreau@sylpheo.com>
2019-01-22 09:28:42 +0100

    First commit

---
+++ IOSCCM2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSCCM2
+//
+//  Created by Jordane SERREAU Dev on 22/01/2019.
+//  Copyright © 2019 Jordane SERREAU Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

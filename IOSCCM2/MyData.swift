434e88128bb84ea0e1f80529f69e69463d2f455f
Serreau Jordane <jordane.serreau@sylpheo.com>
2019-01-23 11:35:24 +0100

    Implement network request management

---
+++ IOSCCM2/MyData.swift
@@ -0,0 +1,9 @@
+//
+//  MyData.swift
+//  IOSCCM2
+//
+//  Created by Jordane SERREAU Dev on 23/01/2019.
+//  Copyright © 2019 Jordane SERREAU Dev. All rights reserved.
+//
+
+import Foundation
---
+++ IOSCCM2/MyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyTableViewCell.swift
+//  IOSCCM2
+//
+//  Created by Jordane SERREAU Dev on 23/01/2019.
+//  Copyright © 2019 Jordane SERREAU Dev. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
